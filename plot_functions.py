e1b14c058e69acc17efd5647853d44cc79ec5b82
unknown <theresa.lang@gmx.net>
2018-08-30 13:54:54 +0200

    Small changes in plot_functions.py, some functions were moved to processing_functions.py.

--- plot_functions.py
+++ plot_functions.py
@@ -11,87 +11,32 @@ import numpy as np
 from os.path import join
 from scipy.interpolate import interp1d
 from matplotlib import dates
-
-
-#############################################
-##Example code to run profile_plot_series
-filename="20180829062417_Grenzschichtentwicklung.npy"
-ref=0                                                               
-p_intv_no=20      
-#############################################
-
-def profile_plot_series(filename,ref,p_intv_no):
-    """
-    Contourf for time series of vertical profiles of temperature/pot. temperatur and humidty for the whole npy. file
-
-    Parameters: 
-        filename (str)  :       name of data file
-        ref             :       Reference Alpaca for time period
-        p_intv_no       :       Number of Pressure levels between boundaries
-        fig_name        :       #Name of figure, using date information
-    """
-    c_p=1005 #J/(kg*K)
-    R_l=287  #J/(kg*K)
-    
-    #Figure name and Plot Title#
-    data_path="//192.168.206.173/lex2018/profil/Daten/"
-    fig_name=filename[:-4]+"profile_complete.png"
-    fig_title="Date "+filename[6:8]+"."+filename[4:6]+"."+filename[0:4]+" ,Start Time: "+filename[8:10]+":"+filename[10:12]+":"+filename[12:14] 
-    
-    ###########################################################################
-    ##Processing of data
-    ###########################################################################
-    data=np.load(data_path+filename)
-    keys=list(data)
-    arduino={}
-    unit_time=data[keys[ref]][1:,0]
-    key_idx= np.asarray(keys)        #ard_number=np.array([1,2,3,4,5,6,7,8,9,10,11])
-    interp_data=np.zeros([len(key_idx),4,len(unit_time)])
-    
-    for i in range(0,len(keys)):
-        for j in range(0,4): # 0 Time, 1 Temp, 2 RH, 3 
-            arduino[keys[i]]=np.asarray(data[keys[i]])
-            interp_data[i,j,:]= interp1d(arduino[keys[i]][1::,0],arduino[keys[i]][1::,j],axis=0,fill_value='extrapolate')(unit_time)
-    print("Data time interpolated")
-    
-    p_min=interp_data[:,3,:].min()
-    p_max=interp_data[:,3,:].max()
-    p_levels=np.linspace(p_min,p_max,p_intv_no)
-    pres_interp=np.zeros([len(p_levels),4,len(unit_time)])
-    for t in range(0,len(unit_time)):
-        for j in range(0,3):
-                pres_interp[:,j,t]=interp1d(interp_data[::,3,t],interp_data[::,j,t],axis=0,fill_value=np.nan,bounds_error=False)(p_levels)
-    print("Data p-interpolated")
-    Temp_pint=pres_interp[:,1,:]
-    RH_pint=pres_interp[:,2,:]
-    
-    #Pot. Temperatur
-    Theta = np.empty((p_intv_no,len(unit_time),))
-    Theta.fill(np.nan)
-    for t in range(0,len(unit_time)):
-        for p in range(0,len(p_levels)):
-            Theta[p,t]=(Temp_pint[p,t]+273.15)*(1000/p_levels[p])**(R_l/c_p)
-            
+from processing_functions import apply_correction
+###############################################################################
+###############################################################################
+def profile_plot_series(filename,server_path,unit_time,p_levels,Temp_pint,RH_pint,Theta):
     ###########################################################################
     ##Plot data
+    fig_name="test"+filename[:-4]+".png"
+    fig_title="Date "+filename[6:8]+"."+filename[4:6]+"."+filename[0:4]+" ,Start Time: "+filename[8:10]+":"+filename[10:12]+":"+filename[12:14] 
     ###########################################################################
     print("Plotting...")
     fig= plt.figure(figsize=(30,15))
     matplotlib.rcParams.update({'font.size': 14})
     
-    levels_t = np.arange(12, 23, 0.5)
-    levels_theta = np.arange(15, 24, 0.5)
-    #######################################
+    levels_T=np.arange(12,23,0.5)
+    levels_Theta=np.arange(14,25,0.5)
+    ###########################################################################
     #Subplot1: Temperatur
     ax1=fig.add_subplot(311)
     X,Y = np.meshgrid(unit_time,p_levels)
-    C= ax1.contourf(X,Y,Temp_pint,levels_t, cmap=plt.get_cmap("hot_r", len(levels_t)-1))
+    C= ax1.contourf(X,Y,Temp_pint,levels_T,cmap=plt.get_cmap("hot_r", len(levels_T)-1),extend="both")
     cb=plt.colorbar(C)
     cb.set_label('Temperatur in $^\circ$C',fontsize=16)
     #
     ax1.set_xticks(ax1.get_xticks()[::])
     ax1.xaxis.set_major_formatter(dates.DateFormatter('%H:%M:%S'))
-    ax1.set_xlim([unit_time[0],date2num(datetime.datetime(2018, 8, 29, 14, 56))])
+    ax1.set_xlim([unit_time[0],unit_time[-1]])
     #ax1.set_xlabel('Local Time')
     ax1.set_ylabel('Pressure in hPa')
     ax1.grid()
@@ -103,19 +48,17 @@ def profile_plot_series(filename,ref,p_intv_no):
     plt.gca().invert_yaxis()
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=None, hspace=0.3)# for space between the subplots  
-    
-    
     ###################################### 
     #Subplot2 pot. Temperatur
     ax2=fig.add_subplot(312)
     X,Y = np.meshgrid(unit_time,p_levels)
-    C2= ax2.contourf(X,Y,Theta-273.15, levels_theta, cmap=plt.get_cmap("hot_r", len(levels_theta)-1))
+    C2= ax2.contourf(X,Y,Theta-273.15,levels_Theta,cmap=plt.get_cmap("hot_r",len(levels_Theta)-1),extend="both")
     cb=plt.colorbar(C2)
     cb.set_label('$\Theta$ in $^\circ$C',fontsize=16)
     
     ax2.set_xticks(ax2.get_xticks()[::])
     ax2.xaxis.set_major_formatter(dates.DateFormatter('%H:%M:%S'))
-    ax2.set_xlim([unit_time[0],date2num(datetime.datetime(2018, 8, 29, 14, 56))])
+    ax2.set_xlim([unit_time[0],unit_time[-1]])
     ax2.grid()
     #ax2.set_xlabel('Local Time')
     ax2.set_ylabel('Pressure in hPa')
@@ -123,7 +66,6 @@ def profile_plot_series(filename,ref,p_intv_no):
     plt.setp(plt.gca().xaxis.get_majorticklabels(),'rotation', 30)
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=None, hspace=0.3)   
-    
     ######################################
     #Subplot3 Relative Humidity
     ax3=fig.add_subplot(313)
@@ -132,7 +74,7 @@ def profile_plot_series(filename,ref,p_intv_no):
     cb.set_label('RH in %',fontsize=16)
     ax3.set_xticks(ax3.get_xticks()[::])
     ax3.xaxis.set_major_formatter(dates.DateFormatter('%H:%M:%S'))
-    ax3.set_xlim([unit_time[0], date2num(datetime.datetime(2018, 8, 29, 14, 56))])
+    ax3.set_xlim([unit_time[0],unit_time[-1]])
     #ax3.set_xlabel('Local Time')
     ax3.set_ylabel('Pressure in hPa')
     ax3.grid()   
@@ -141,18 +83,13 @@ def profile_plot_series(filename,ref,p_intv_no):
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=None, hspace=0.3)
     fig.savefig(fig_name, dpi=500,bbox_inches='tight')
-    server_path="//192.168.206.173//lex2018/profil/Plots/"
     fig.savefig(server_path+fig_name,dpi=500,bbox_inches="tight")
-    
-    print("Plotted and stored on server")
     plt.close()
-    return Theta,p_levels,Temp_pint,unit_time;
-    ###########################################################################
-    ###########################################################################
+    print("Plotted and stored on server")
+    return        
 
-            
-
-
+###############################################################################
+###############################################################################
 
 def profilplot(path, filename, time_start, time_end):
     """
@@ -198,21 +135,8 @@ def profilplot(path, filename, time_start, time_end):
     plt.tight_layout()
 
 
-
-#==============================================================================
-#i=0
-#while True:
-#     time.sleep(1)
-#     i += 1
-#     print(i)
-#     if np.mod(i,60) == 0 :
-         #run function profile_plot_series
-#         try:    
-#Theta,p_levels,Temp_pint, unit_time = profile_plot_series(filename,ref,p_intv_no)
-#         except:
-#             print('NOPE...')
-#==============================================================================
-    
+###############################################################################
+###############################################################################
 def plot_timeseries(path, filename, start_time=None, end_time=None, t_range=None,
                     h_range=None, p_range=None):
     """ Plots a timeseries of ALPACA data for the time period between start_time
@@ -294,49 +218,3 @@ def plot_timeseries(path, filename, start_time=None, end_time=None, t_range=None
 
     plt.tight_layout()
 
-def altitude(pressure, temperature):
-    """ Calculates altitude fromm pressure and temperature.
-    
-    Parameters:
-        pressure (array): vertical pressure profile 
-        temperature (array): vertical temperature profile
-    """
-    
-    # constants
-    R = 287.058
-    g = 9.81
-    z = np.zeros(len(pressure))
-    z_interv = np.zeros(len(pressure))
-    z0 = 7.0
-
-    for lev in range(0, len(pressure)-1):
-        z_interv[lev+1] = np.log(pressure[lev+1] / pressure[lev]) * -(R * (temperature[lev] + temperature[lev+1]) / 2 / g)
-        
-        z[lev+1] = np.sum(z_interv)
-        
-    return z + z0
-
-def apply_correction(path, filename):
-    """ Applies temperature and himidity correction to ALPACA raw data and 
-    returns a dictionary with the same structure as in the raw data, but 
-    with the corrected values. 
-    """
-    
-    data = np.load(path + filename)
-    arduinos = data.keys()
-    
-    temp_correction = {1: 0.09, 2: 0.10, 3: -0.02, 4: -0.23, 5: -0.20,
-                       6: 0.05, 7: 0.14, 8: 0.11, 9: -0.10, 10: 0.11,
-                       11: -0.09}
-    
-    humidity_correction = {1: -0.10, 2: 0.34, 3: -0.03, 4: 0.17, 5: 0.48,
-                       6: -0.14, 7: -2.09, 8: 1.07, 9: -0.60, 10: -0.30,
-                       11: 2.00}
-    
-    for i in arduinos:
-        # temperature
-        data[i][1:, 1] = data[i][1:, 1] + temp_correction[i]
-        # humidity
-        data[i][1:, 2] = data[i][1:, 2] + humidity_correction[i]
-        
-    return data
