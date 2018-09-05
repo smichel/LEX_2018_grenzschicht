65e4dec32306d5a1f777d7dd080f9a9d5aaa21a7
Simon Michel <simon.michel.j@gmail.com>
2018-09-05 13:49:03 +0200

    cosmo updates

--- cosmo.py
+++ cosmo.py
@@ -7,17 +7,19 @@ from processing_functions import calc_pot_temp
 import typhon
 import datetime
 dataFilePath = 'C:/Users/Uni/Desktop/CosmoDE/cd2_feh_2018090400.nc'
+alpaca = np.load('//192.168.206.173/lex2018/profil/Daten/20180904124938_Grenzschichtentwicklung5.npy')
+
 staticGridPath = 'C:/Users/Uni/Desktop/CosmoDE/cd2_feh_2018082900c.nc'
 data = netCDF4.Dataset(dataFilePath)
 staticGrid = netCDF4.Dataset(staticGridPath)
 # Marienleuchte bei 11/10
-
+maxlevel = -22
 landfraction = staticGrid.variables['FR_LAND'][:]
 time = data.variables['time'][:]
 timestring = data.variables['time'].units
 date = datetime.datetime(int(data.variables['time'].units[12:16]),int(data.variables['time'].units[17:19]),int(data.variables['time'].units[20:22]),int(data.variables['time'].units[23:25]))
 hhl = staticGrid.variables['HHL'][:]
-heights = hhl[0,-17:-1,11,10]
+heights = hhl[0,maxlevel:-1,11,10]
 P = data.variables['P'][:]
 T = data.variables['T'][:]
 T_2M = data.variables['T_2M'][:]
@@ -40,27 +42,37 @@ def cosmoprofil(variable): #nur für 1D und 2D sachen du otto
             for j in range(variable.shape[1]-1):
                 interpVariable[i,j] = 0.5*(variable[i,j]+variable[i,j+1])
         return interpVariable
-heights = cosmoprofil(heights)
-pres = cosmoprofil(P[:,-17:-1,11,10]).T
-Tprofile = cosmoprofil(T[:,-17:-1,11,10]).T
-Tprofile = Tprofile-typhon.constants.zero_celsius
 
-potT = calc_pot_temp(Tprofile, pres/100)
-def plot_2d_timeseries(variable):
+def plot_2d_timeseries(variable,caxismin=None, caxismax=None):
+    if caxismin == None:
+        caxismin = np.floor(np.min(variable))
+    if caxismax == None:
+        caxismax = np.ceil(np.max(variable))
+
     f = plt.figure(frameon=True)
     ax = f.add_subplot(111)
     #for i in range(T.shape[0]):
-    var1 = ax.imshow(variable)
+    var1 = ax.imshow(variable,vmin=caxismin,vmax=caxismax)
     #ax.set_xticks(np.arange(0,16,2))
-    ax.set_yticks(np.arange(0,16,2))
+    ax.set_yticks(np.arange(0,variable.shape[0],2))
     ax.set_xticklabels(date[::4])
     plt.setp(plt.gca().xaxis.get_majorticklabels(),'rotation', 30)
     ax.set_yticklabels(np.round(heights[::2]))
     ax.set_xlabel('Date')
     ax.set_ylabel('Height in m')
-    cb = plt.colorbar(var1)
+    cb = plt.colorbar(var1)#,boundaries=np.arange(caxismin,caxismax,(caxismax-caxismin)/10))
     plt.show(block=False)
+
+heights = cosmoprofil(heights)
+pres = cosmoprofil(P[:,maxlevel:-1,11,10]).T
+Tprofile = cosmoprofil(T[:,maxlevel:-1,11,10]).T
+Tprofile = Tprofile-typhon.constants.zero_celsius
+
+potT = calc_pot_temp(Tprofile, pres/100)
+
+
 plot_2d_timeseries(potT-typhon.constants.zero_celsius)
 plot_2d_timeseries((np.diff(potT,axis=0)*100)/np.diff(heights)[:,None])
 plot_2d_timeseries(Tprofile)
+plot_2d_timeseries(cosmoprofil(RH[:,maxlevel:-1,11,10]).T, 0, 100)
 print(1)
