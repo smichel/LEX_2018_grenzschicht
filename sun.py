a397c5576bb51de4b26dffe581f8dc582d73e09a
Simon Michel <simon.michel.j@gmail.com>
2018-09-14 17:05:10 +0200

    fixes for stupid mistakes

--- sun.py
+++ sun.py
@@ -2,10 +2,7 @@ from pysolar.solar import *
 import numpy as np
 import datetime
 from matplotlib.dates import date2num, num2date
-
-dtype1=np.dtype([('Time','f8'),('Latitude','f8'),('Longitude','f8'),('Elevation','f8')])
-
-data = np.loadtxt('/data/share/u231/u231101/lehre/lex2018/profil/Daten/Auslenkung_Leine/20180831105131_Grenzschichtentwicklung2_gps.txt',dtype=dtype1,skiprows=1,delimiter=';')
+import matplotlib.pyplot as plt
 
 def strahlungsfehler(times, gott_file, base_location = (54.494849, 11.2400009)):
     '''
@@ -32,7 +29,9 @@ def strahlungsfehler(times, gott_file, base_location = (54.494849, 11.2400009)):
     for i in range(len(data)):
         time[i] = data[i][0]
         sun_azimuth = get_azimuth(base_location[0], base_location[1], num2date(data[i][0]))
-        gott_azi[i] = np.rad2deg(np.arctan2((base_location[0] - data[i][1]), (base_location[1] - data[i][2])))
+        x = -(base_location[0] - data[i][1])*np.cos(np.deg2rad(base_location[0]))
+        y = -(base_location[1] - data[i][2])
+        gott_azi[i] = (np.rad2deg(np.arctan2(x,y))+360)%360
 
         if sun_azimuth < 0:
             if (sun_azimuth >= -180):
@@ -42,11 +41,13 @@ def strahlungsfehler(times, gott_file, base_location = (54.494849, 11.2400009)):
             if sun_azimuth >= 0:
                 solarheading = sun_azimuth
 
+
+
         sun_azi[i] = solarheading
 
         if i > 0:
             if np.abs(gott_azi[i]-gott_azi[i-1])>1:
-                gott_azi[i] = gott_azi[i-1]
+                gott_azi[i] = np.nan
 
     sun = np.zeros(len(data))
     sun[np.abs(sun_azi-gott_azi)<20] = 1
@@ -56,10 +57,10 @@ def strahlungsfehler(times, gott_file, base_location = (54.494849, 11.2400009)):
     for i in range(len(intime)):
         idx[i] = np.argmax(intime[i]<time)
 
-    sun_out = np.zeros(len(data))*np.nan
+    sun_out = np.zeros(len(intime))*np.nan
 
     for i in range(len(sun_out)):
         if idx[i] > 0:
-            sun_out[i] = sun[idx[i]]
+            sun_out[i] = sun[np.int(idx[i])]
 
     return sun_out
